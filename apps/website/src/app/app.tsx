2dc388bb8f0568b5ec23d8f2a6ea98f314e904c2
usaEpoxy <usaepoxyfloor@gmail.com>
2022-03-07 11:53:24 -0500

    initial commit

--- apps/website/src/app/app.tsx
+++ apps/website/src/app/app.tsx
@@ -1,14 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
-import NxWelcome from './nx-welcome';
 
 export function App() {
-  return (
-    <>
-      <NxWelcome title="website" />
-      <div />
-    </>
-  );
+  return <div />;
 }
 
 export default App;
